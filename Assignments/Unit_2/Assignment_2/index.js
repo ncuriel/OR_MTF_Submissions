9aa8d0807d9ece0c0c10a746c195b280324edcd9
Nemo C <ncuriel@gmail.com>
2018-05-29 17:19:22 -0700

    completed assignment 2 unit 2

--- Assignments/Unit_2/Assignment_2/index.js
+++ Assignments/Unit_2/Assignment_2/index.js
@@ -1,5 +1,27 @@
-// Write Javascript to perform the following tasks. Please submit links to your assignment on repl.it. Here is the Javascript console we used in class: repl.it
+/*
+Write Javascript to perform the following tasks. 
+Please push your changes back to your personal OR_MTF_Submissions directory. 
+*/
+
+// USE JavaSCript to complege these tasks:
 
 // 1. Change the background color of The first page by selecting the appropriate ID.
+document.querySelector("body").style.backgroundColor = "lightblue";
 
-// 2.
+// 2. Change the inner text of the h1 to say anything that you would like.
+document.getElementById("title").innerHTML = "Hola Mundo!";
+
+// 3. Create a variable that is equal to the DOM object that has the id random-btn.
+let button = document.getElementById("random-btn");
+
+// 4. Create a variable that is equal to the DOM object that has the id pageTitle. 
+let someTitle = document.getElementById("pageTitle");
+
+// 5. Create a fucntion called randomSize that changes the font size of pageTitle to be between 1px and 100px.
+function randomSize() {
+	let size = Math.floor(Math.random()*100);
+	document.getElementById("pageTitle").style.fontSize = "size";
+}
+
+// 6. Create an event listener that runs the randomSize function on click.
+document.querySelector("body").onclick = function() {randomSize()};
