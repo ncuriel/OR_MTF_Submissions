b6c16436e30c888768467b13d79373890c33faab
Nemo C <ncuriel@gmail.com>
2018-06-25 16:48:16 -0700

    fixed mistakes on js file

--- Assignments/Unit_2/Assignment_2/index.js
+++ Assignments/Unit_2/Assignment_2/index.js
@@ -1,6 +1,6 @@
 /*
-Write Javascript to perform the following tasks. 
-Please push your changes back to your personal OR_MTF_Submissions directory. 
+Write Javascript to perform the following tasks.
+Please push your changes back to your personal OR_MTF_Submissions directory.
 */
 
 // USE JavaSCript to complege these tasks:
@@ -9,19 +9,19 @@ Please push your changes back to your personal OR_MTF_Submissions directory.
 document.querySelector("body").style.backgroundColor = "lightblue";
 
 // 2. Change the inner text of the h1 to say anything that you would like.
-document.getElementById("title").innerHTML = "Hola Mundo!";
+document.getElementById("PageTitle").innerHTML = "Hola Mundo!";
 
 // 3. Create a variable that is equal to the DOM object that has the id random-btn.
-let button = document.getElementById("random-btn");
+let button = document.getElementById('random-btn');
 
-// 4. Create a variable that is equal to the DOM object that has the id pageTitle. 
-let someTitle = document.getElementById("pageTitle");
+// 4. Create a variable that is equal to the DOM object that has the id pageTitle.
+let someTitle = document.getElementById('PageTitle');
 
 // 5. Create a fucntion called randomSize that changes the font size of pageTitle to be between 1px and 100px.
 function randomSize() {
 	let size = Math.floor(Math.random()*100);
-	document.getElementById("pageTitle").style.fontSize = "size";
+	return someTitle.style.fontSize = `${size}px`;
 }
 
 // 6. Create an event listener that runs the randomSize function on click.
-document.querySelector("body").onclick = function() {randomSize()};
+button.addEventListener('click', randomSize);
